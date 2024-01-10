ed4922ace6ddb9045ccbb192ad79716454d03325
Lars Reuter <lp.reuter@gmail.com>
2024-01-10 21:24:36 +0100

    Extract image URLs and push them into array

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ import cheerio from 'cheerio';
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 async function fetchWebsite() {
+  // Fetch website html (string) asynchronously.
   const request = await fetch(website, { cache: 'no-cache' });
   const result = await request.text();
   return result;
 }
 
 async function processWebsite() {
-  const text = await fetchWebsite();
-  // const html = parseFromString(text);
-  const html = cheerio.load(text);
-  const img = html('#images').html();
-  console.log(img);
+  // Await for fetchWebsite before loading HTML string into Cheerio string to HTML parser.
+  const textContent = await fetchWebsite();
+  const $ = cheerio.load(textContent);
+
+  // Iterate through all <img> tags, extract the "src" attribute and push them into imageArr array.
+  const imageArr = [];
+  $('img').each(function (index, element) {
+    imageArr.push($(element).attr('src'));
+  });
+
+  // Log imageArr to console for reference.
+  console.log(imageArr);
 }
 
 processWebsite();
