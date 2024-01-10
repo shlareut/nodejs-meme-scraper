74661629d5a7f00f98d48e3e31b8e97ed34d15c8
Lars Reuter <lp.reuter@gmail.com>
2024-01-10 22:03:26 +0100

    Refactor code and init function to fetch raw image data

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 import cheerio from 'cheerio';
 
+// Create constant with website that should be scraped.
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-async function fetchWebsite() {
+async function fetchWebsiteTextContent() {
   // Fetch website html (string) asynchronously.
-  const request = await fetch(website, { cache: 'no-cache' });
-  const result = await request.text();
-  return result;
+  const getWebsiteTextContent = await fetch(website, { cache: 'no-cache' });
+  const websiteTextContent = await getWebsiteTextContent.text();
+  return websiteTextContent;
 }
 
-async function extractImages() {
+async function extractImageUrls() {
   // Await for fetchWebsite before loading HTML string into Cheerio string to HTML parser.
-  const textContent = await fetchWebsite();
-  const $ = cheerio.load(textContent);
+  const websiteTextContent = await fetchWebsiteTextContent();
+  const $ = cheerio.load(websiteTextContent);
 
   // Iterate through all <img> tags, extract the "src" attribute and push them into imageArr array.
-  const imageArr = [];
+  const imagesArray = [];
   $('img').each(function (index, element) {
-    imageArr.push($(element).attr('src'));
+    imagesArray.push($(element).attr('src'));
   });
 
   // Return image array to be used in next function
-  return imageArr;
+  return imagesArray;
 }
 
-async function processImages() {
-  const images = await extractImages();
-  const topTenImages = images.slice(0, 10);
+async function processImagesArray() {
+  // Await image extraction to be completed.
+  const imagesArray = await extractImageUrls();
+
+  // Create new array consisting of the first ten images.
+  const topTenImages = imagesArray.slice(0, 10);
+
+  // Return top ten images array for data extraction in the next function.
+  return topTenImages;
+}
+
+async function fetchRawImageData() {
+  // Await output from array processing function.
+  const topTenImages = await processImagesArray();
+
+  // TODO: Iterate through the array, fetch the data and push into new array.
+  //
+  //
+  //
+
+  // Testing on one image file.
+  // const getImageData
+
   console.log(topTenImages);
 }
 
-processImages();
+fetchRawImageData();
