32801106ec692ba2d2e2b81b6fed54c1806c2006
Lars Reuter <lp.reuter@gmail.com>
2024-01-10 18:58:49 +0100

    Change DOM parser and fix /module issue

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import { parseFromString } from 'dom-parser';
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 async function fetchWebsite() {
-  let request = await fetch(website, { cache: 'no-cache' });
-  let result = await request.text();
+  const request = await fetch(website, { cache: 'no-cache' });
+  const result = await request.text();
   return result;
 }
 
 async function processWebsite() {
-  let body = await fetchWebsite();
-  let HTMLBody = parseFrom;
-  console.log(body);
+  const text = await fetchWebsite();
+  const html = parseFromString(text);
+  console.log(html);
+  // console.log(html.querySelector('img'));
 }
 
 processWebsite();
