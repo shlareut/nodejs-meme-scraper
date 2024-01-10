f5f4a1d2db4048533fac98082f24ae7029584bf4
Lars Reuter <lp.reuter@gmail.com>
2024-01-10 18:16:10 +0100

    Add DOM parser package

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ import { parseFromString } from 'dom-parser';
 
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-new XMLHttpRequest();
+async function fetchWebsite() {
+  let request = await fetch(website, { cache: 'no-cache' });
+  let result = await request.text();
+  return result;
+}
 
-// async function fetchWebsite() {
-//   let request = await fetch(website, { cache: 'no-cache' });
-//   let result = await request.text();
-//   return result;
-// }
+async function processWebsite() {
+  let body = await fetchWebsite();
+  let HTMLBody = parseFrom;
+  console.log(body);
+}
 
-// async function processWebsite() {
-//   let body = await fetchWebsite();
-//   let HTMLBody = parseFrom;
-//   console.log(body);
-// }
-
-// processWebsite();
+processWebsite();
