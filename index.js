c8cd4cb11803339b42221367ec8daaf46f827039
Lars Reuter <lp.reuter@gmail.com>
2024-01-11 19:28:52 +0100

    Add back package solution to see if test passes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as fs from 'node:fs/promises'; // Promise-based Node.js module to enable interactions with the file system
+// import * as fs from 'node:fs/promises'; // Promise-based Node.js module to enable interactions with the file system
 import cheerio from 'cheerio'; // Library for parsing and manipulating HTML
-import { encode } from 'node-base64-image'; // Library to extract Base64 from image URLs
+import { decode, encode } from 'node-base64-image'; // Library to extract Base64 from image URLs
 
 // Create constant with website that should be scraped.
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
@@ -23,21 +23,29 @@ parseToHTML('img').each(function (index, element) {
 // Create new array consisting of the first ten images.
 const topTenImagesURLs = imagesArray.slice(0, 10);
 
-// Create empty array for storing base64 image data
-const topTenImagesData = [];
+// // Create empty array for storing base64 image data
+// const topTenImagesData = [];
 
-// Loop through image URLs, extract base64 image data and push to empty array.
-for (const i of topTenImagesURLs) {
-  const imageData = await encode(i);
-  await topTenImagesData.push(imageData);
-}
+// // Loop through image URLs, extract base64 image data and push to empty array.
+// for (const i of topTenImagesURLs) {
+//   const imageData = await encode(i);
+//   await topTenImagesData.push(imageData);
+// }
 
-// Initialize a counter for naming the image files.
+// // Initialize a counter for naming the image files.
+// let imageCount = 1;
+
+// // Loop through base64 image array and create .jpg files for each, increase counter for naming.
+// for (const i of topTenImagesData) {
+//   imageCount = imageCount < 10 ? '0' + imageCount : imageCount;
+//   await fs.writeFile(`./memes/${imageCount}.jpg`, i);
+//   imageCount++;
+// }
+
 let imageCount = 1;
-
-// Loop through base64 image array and create .jpg files for each, increase counter for naming.
-for (const i of topTenImagesData) {
+for (const i of topTenImagesURLs) {
   imageCount = imageCount < 10 ? '0' + imageCount : imageCount;
-  await fs.writeFile(`./memes/${imageCount}.jpg`, i);
+  const image = await encode(i);
+  await decode(image, { fname: `./memes/${imageCount}`, ext: 'jpg' });
   imageCount++;
 }
